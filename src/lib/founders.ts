7964c365ad03e5ca6b058723fa6faefe1404b8f1
Doruk Yalçın <dyalcin21@ku.edu.tr>
2026-03-26 02:35:35 +0300

    Raise Doruk portrait within circular crop

--- src/lib/founders.ts
+++ src/lib/founders.ts
@@ -16,7 +16,7 @@ export const founders = [
     photo: {
       src: "/founders/doruk.jpg",
       alt: "Doruk Yalcin, co-founder of Avernsys",
-      objectPosition: "78% 30%",
+      objectPosition: "78% 42%",
       scale: 1.95,
     },
   },
