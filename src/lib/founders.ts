08d93be10eec40b0f0588df63ba2caf8a16dc21e
Doruk Yalçın <dyalcin21@ku.edu.tr>
2026-03-26 02:30:10 +0300

    Set Doruk portrait crop to 30 percent

--- src/lib/founders.ts
+++ src/lib/founders.ts
@@ -16,7 +16,7 @@ export const founders = [
     photo: {
       src: "/founders/doruk.jpg",
       alt: "Doruk Yalcin, co-founder of Avernsys",
-      objectPosition: "78% 34%",
+      objectPosition: "78% 30%",
       scale: 1.95,
     },
   },
